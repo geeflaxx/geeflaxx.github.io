ca2ecfa87e90eef3d14bce7ce382f3e251b99515
Patrick Seemann <patrick-seemann@hotmail.de>
2024-09-30 21:49:43 +0200

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -1,20 +1,13 @@
-
- //Custom Text Header //
- // ACSII slant font
-
-// Needs to be inside <section id="login" class="shadow">
-// YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
-
-let custom_text_header = "
-<div class=\"custom-text-header\"> <pre> 
+let custom_text_header = `
+<div class="custom-text-header"> <pre> 
 ______     _        _      _      _____                                       
-| ___ \   | |      (_)    | |    /  ___|                                      
-| |_/ /_ _| |_ _ __ _  ___| | __ \ `--.  ___  ___ _ __ ___   __ _ _ __  _ __  
-|  __/ _` | __| '__| |/ __| |/ /  `--. \/ _ \/ _ \ '_ ` _ \ / _` | '_ \| '_ \ 
-| | | (_| | |_| |  | | (__|   <  /\__/ /  __/  __/ | | | | | (_| | | | | | | |
-\_|  \__,_|\__|_|  |_|\___|_|\_\ \____/ \___|\___|_| |_| |_|\__,_|_| |_|_| |_|
+| ___ \\   | |      (_)    | |    /  ___|                                      
+| |_/ /_ _| |_ _ __ _  ___| | __ \\ \`--.  ___  ___ _ __ ___   __ _ _ __  _ __  
+|  __/ _\` | __| '__| |/ __| |/ /  \`--. \\/ _ \\/ _ \\ '_ \` _ \\ / _\` | '_ \\| '_ \\ 
+| | | (_| | |_| |  | | (__|   <  /\\__/ /  __/  __/ | | | | | (_| | | | | | | |
+\\_|  \\__,_|\\__|_|  |_|\\___|_|\\_\\ \\____/ \\___|\\___| | |_| |_|\\__,_|_| |_|_| |_|
                                                                               
                                                                            
                                                                  </pre> </div>
-";
-document.getElementById("login").innerHTML += custom_text_header
+`;
+document.getElementById("login").innerHTML += custom_text_header;
